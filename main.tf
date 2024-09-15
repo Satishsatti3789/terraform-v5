289351cd7aa96621c011307d925e7abd9495c5c2
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-15 06:43:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,22 @@ module "instances" {
   for_each = var.instances
   source   = "./ec2"
   name     = each.key
+  type     = each.value
 }
 
 variable "instances" {
   default = {
 
-    frontend  = {}
-    mongodb   = {}
-    catalogue = {}
-    redis     = {}
-    user      = {}
-    cart      = {}
-    mysql     = {}
-    shipping  = {}
-    rabbitmq  = {}
-    payment   = {}
+    frontend  = t2.medium
+    mongodb   = t2.medium
+    catalogue = t2.micro
+    redis     = t2.small
+    user      = t2.small
+    cart      = t2.medium
+    mysql     = t2.medium
+    shipping  = t2.micro
+    rabbitmq  = t2.micro
+    payment   = t2.small
 
   }
 }
