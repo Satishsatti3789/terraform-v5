7c9517df5a44e0506b5827ab8f29a10a2e2a7a47
Satishsatti3789 <satishdevops3789@gmail.com>
2024-09-15 06:55:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,18 +6,18 @@ module "instances" {
 }
 
 variable "instances" {
+  type        = map(string)
   default = {
-
-    frontend  = t2.medium
-    mongodb   = t2.medium
-    catalogue = t2.micro
-    redis     = t2.small
-    user      = t2.small
-    cart      = t2.medium
-    mysql     = t2.medium
-    shipping  = t2.micro
-    rabbitmq  = t2.micro
-    payment   = t2.small
-
+    frontend  = "t2.medium"
+    mongodb   = "t2.medium"
+    catalogue = "t2.micro"
+    redis     = "t2.small"
+    user      = "t2.small"
+    cart      = "t2.medium"
+    mysql     = "t2.medium"
+    shipping  = "t2.micro"
+    rabbitmq  = "t2.micro"
+    payment   = "t2.small"
   }
+}
 }
